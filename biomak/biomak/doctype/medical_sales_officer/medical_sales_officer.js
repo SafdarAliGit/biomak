de55c8f738d42a800524acb1322ba2047f7860d2
safdar211ali <safdar211@hotmail.com>
2024-03-06 22:33:06 +0500

    updated

---
+++ biomak/biomak/doctype/medical_sales_officer/medical_sales_officer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, TechVentures and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Medical Sales Officer', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ biomak/biomak/doctype/regional_manager/regional_manager.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, TechVentures and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Regional Manager', {
+	// refresh: function(frm) {
+
+	// }
+});
