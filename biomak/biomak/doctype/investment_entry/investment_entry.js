5f940e14a4093d4b4370c41ef4117c1e59c3ae59
safdar211ali <safdar211@hotmail.com>
2025-01-16 19:14:41 +0500

    updated

--- biomak/biomak/doctype/investment_entry/investment_entry.js
+++ biomak/biomak/doctype/investment_entry/investment_entry.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Investment Entry', {
-	// refresh: function(frm) {
-
-	// }
+    // Add your parent doctype refresh or other event handlers here if needed
 });
+
+frappe.ui.form.on('Investment Entry Items', {
+    investment_amount: function (frm, cdt, cdn) {
+        calculate_sale_target(frm, cdt, cdn);
+    },
+    sale_multiplier: function (frm, cdt, cdn) {
+        calculate_sale_target(frm, cdt, cdn);
+    }
+});
+
+function calculate_sale_target(frm, cdt, cdn) {
+    let row = locals[cdt][cdn]; // Access the current row
+    let investment_amount = row.investment_amount || 0; // Default to 0 if undefined
+    let sale_multiplier = row.sale_multiplier || 0; // Default to 0 if undefined
+    let sale_target = investment_amount * sale_multiplier; // Calculate sale target
+    frappe.model.set_value(cdt, cdn, "sale_target", sale_target); // Update the field
+}
