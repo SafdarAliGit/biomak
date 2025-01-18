673774fbfb8cbab4d582a5b0691f1e52158508e4
safdar211ali <safdar211@hotmail.com>
2025-01-18 20:41:36 +0500

    updated

--- biomak/biomak/report/investment_and_sales_plan/investment_and_sales_plan.py
+++ biomak/biomak/report/investment_and_sales_plan/investment_and_sales_plan.py
@@ -11,36 +11,46 @@ def execute(filters=None):
 
 
 def get_columns():
-    columns = [
-        {
-            "label": "<b>PLAN</b>",
-            "fieldname": "plan",
-            "fieldtype": "Data",
-            "width": 100
+    columns = fields = [
+        {"label": "Doctor", "fieldname": "doctor", "fieldtype": "Data", "width": 150},
+        {"label": "Town", "fieldname": "town", "fieldtype": "Data", "width": 120},
+        {"label": "MSO", "fieldname": "mso", "fieldtype": "Data", "width": 120},
+        {"label": "RM", "fieldname": "rm", "fieldtype": "Data", "width": 120},
+        {"label": "Medical Store", "fieldname": "medical_store", "fieldtype": "Data", "width": 180},
 
-        },
-        {
-            "label": "<b>MSO</b>",
-            "fieldname": "mso",
-            "fieldtype": "Data",
-            "width": 100
+        # Monthly Investment Fields
+        {"label": "Investment", "fieldname": "investment_amount", "fieldtype": "Currency", "width": 120},
 
-        },
-        {
-            "label": "<b>RM</b>",
-            "fieldname": "rm",
-            "fieldtype": "Data",
-            "width": 100
 
-        }
+        # Monthly Sales Fields
+        {"label": "Sales", "fieldname": "sale_amount", "fieldtype": "Currency", "width": 120},
+        {"label": "Target", "fieldname": "sale_target", "fieldtype": "Currency", "width": 120},
+
     ]
+
     return columns
 
 
 def get_data(filters):
     data = []
-    investment_entry = frappe.db.sql(
-        "Select plan,mso,rm from `tabInvestment Entry`",
-    )
-    data.extend(investment_entry)
+    query = """
+    SELECT
+    i.doctor AS doctor,
+    i.town AS town,
+    i.mso AS mso,
+    i.rm AS rm,
+    i.sale_target,
+    -- Sum for each month from Investment Entry Items
+    i.amount As investment_amount,
+    -- Sum for each month from Sales Entry Items
+    s.amount AS sale_amount
+    FROM
+        `tabInvestment Entry Items` i
+    JOIN 
+        `tabSales Entry Items` s ON i.doctor = s.doctor
+    GROUP BY
+        i.doctor, i.town, i.mso, i.rm
+    """
+    query_result = frappe.db.sql(query, as_dict=True)
+    data.extend(query_result)
     return data
