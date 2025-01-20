cc5fdb5710bd5290b7b15bda4453c54ff37d21b4
safdar211ali <safdar211@hotmail.com>
2025-01-20 20:38:53 +0500

    updated

--- biomak/biomak/report/investment_and_sales_plan/investment_and_sales_plan.py
+++ biomak/biomak/report/investment_and_sales_plan/investment_and_sales_plan.py
@@ -31,8 +31,8 @@ def get_columns():
         {"label": "I-OCT", "fieldname": "october_investment", "fieldtype": "Data", "width": 120},
         {"label": "I-NOV", "fieldname": "november_investment", "fieldtype": "Data", "width": 120},
         {"label": "I-DEC", "fieldname": "december_investment", "fieldtype": "Data", "width": 120},
-        {"label":"Tot. Inv", "fieldname": "total_investment", "fieldtype": "Data", "width": 120},
-        {"label":"Tot. Target", "fieldname": "total_sale_target", "fieldtype": "Data", "width": 120},
+        {"label": "Tot. Inv", "fieldname": "total_investment", "fieldtype": "Data", "width": 120},
+        {"label": "Tot. Target", "fieldname": "total_sale_target", "fieldtype": "Data", "width": 120},
 
         # Monthly Sales Fields
         {"label": "S-JAN", "fieldname": "january_sales", "fieldtype": "Data", "width": 120},
@@ -47,59 +47,112 @@ def get_columns():
         {"label": "S-OCT", "fieldname": "october_sales", "fieldtype": "Data", "width": 120},
         {"label": "S-NOV", "fieldname": "november_sales", "fieldtype": "Data", "width": 120},
         {"label": "S-DEC", "fieldname": "december_sales", "fieldtype": "Data", "width": 120},
-        {"label":"Tot. Sales", "fieldname": "total_sale", "fieldtype": "Data", "width": 120},
-        {"label":"Remaining Sale", "fieldname": "remaining_sale", "fieldtype": "Data", "width": 120},
+        {"label": "Tot. Sales", "fieldname": "total_sale", "fieldtype": "Data", "width": 120},
+        {"label": "Remaining Sale", "fieldname": "remaining_sale", "fieldtype": "Data", "width": 120},
+        {"label": "Remaining Amount", "fieldname": "remaining_amount", "fieldtype": "Data", "width": 120},
     ]
 
     return columns
 
 
+def get_conditions(filters):
+    """
+    Constructs the WHERE clause conditions based on the filters provided.
+
+    Args:
+        filters (dict): A dictionary of filter keys and values.
+
+    Returns:
+        str: A string representing the WHERE clause conditions.
+        dict: A dictionary of parameters for the query.
+    """
+    conditions = []
+    parameters = {}
+
+    if filters.get("doctor"):
+        conditions.append("i.doctor = %(doctor)s")
+        parameters["doctor"] = filters["doctor"]
+
+    if filters.get("mso"):
+        conditions.append("i.mso = %(mso)s")
+        parameters["mso"] = filters["mso"]
+
+    if filters.get("rm"):
+        conditions.append("i.rm = %(rm)s")
+        parameters["rm"] = filters["rm"]
+
+    where_clause = " AND ".join(conditions) if conditions else "1=1"
+    return where_clause, parameters
+
+
 def get_data(filters):
-    data = []
-    query = """
+    """
+    Fetches investment and sales data based on provided filters.
+
+    Args:
+        filters (dict): A dictionary of filter keys and values.
+
+    Returns:
+        list: A list of dictionaries containing aggregated data.
+    """
+    where_clause, parameters = get_conditions(filters)
+
+    query = f"""
     SELECT
-    i.doctor AS doctor,
-    i.town AS town,
-    i.mso AS mso,
-    i.rm AS rm,
-    i.medical_store AS medical_store,
-    -- Sum for each month from Investment Entry Items
-    SUM(CASE WHEN i.month = 'January' THEN i.investment_amount ELSE 0 END) AS january_investment,
-    SUM(CASE WHEN i.month = 'February' THEN i.investment_amount ELSE 0 END) AS february_investment,
-    SUM(CASE WHEN i.month = 'March' THEN i.investment_amount ELSE 0 END) AS march_investment,
-    SUM(CASE WHEN i.month = 'April' THEN i.investment_amount ELSE 0 END) AS april_investment,
-    SUM(CASE WHEN i.month = 'May' THEN i.investment_amount ELSE 0 END) AS may_investment,
-    SUM(CASE WHEN i.month = 'June' THEN i.investment_amount ELSE 0 END) AS june_investment,
-    SUM(CASE WHEN i.month = 'July' THEN i.investment_amount ELSE 0 END) AS july_investment,
-    SUM(CASE WHEN i.month = 'August' THEN i.investment_amount ELSE 0 END) AS august_investment,
-    SUM(CASE WHEN i.month = 'September' THEN i.investment_amount ELSE 0 END) AS september_investment,
-    SUM(CASE WHEN i.month = 'October' THEN i.investment_amount ELSE 0 END) AS october_investment,
-    SUM(CASE WHEN i.month = 'November' THEN i.investment_amount ELSE 0 END) AS november_investment,
-    SUM(CASE WHEN i.month = 'December' THEN i.investment_amount ELSE 0 END) AS december_investment,
-    COALESCE(SUM(i.investment_amount),0) AS total_investment,
-    COALESCE(SUM(i.sale_target),0) AS total_sale_target,
-    
-    -- Sum for each month from Sales Entry Items
-    SUM(CASE WHEN i.month = 'January' THEN i.sale_amount ELSE 0 END) AS january_sales,
-    SUM(CASE WHEN i.month = 'February' THEN i.sale_amount ELSE 0 END) AS february_sales,
-    SUM(CASE WHEN i.month = 'March' THEN i.sale_amount ELSE 0 END) AS march_sales,
-    SUM(CASE WHEN i.month = 'April' THEN i.sale_amount ELSE 0 END) AS april_sales,
-    SUM(CASE WHEN i.month = 'May' THEN i.sale_amount ELSE 0 END) AS may_sales,
-    SUM(CASE WHEN i.month = 'June' THEN i.sale_amount ELSE 0 END) AS june_sales,
-    SUM(CASE WHEN i.month = 'July' THEN i.sale_amount ELSE 0 END) AS july_sales,
-    SUM(CASE WHEN i.month = 'August' THEN i.sale_amount ELSE 0 END) AS august_sales,
-    SUM(CASE WHEN i.month = 'September' THEN i.sale_amount ELSE 0 END) AS september_sales,
-    SUM(CASE WHEN i.month = 'October' THEN i.sale_amount ELSE 0 END) AS october_sales,
-    SUM(CASE WHEN i.month = 'November' THEN i.sale_amount ELSE 0 END) AS november_sales,
-    SUM(CASE WHEN i.month = 'December' THEN i.sale_amount ELSE 0 END) AS december_sales,
-    COALESCE(SUM(i.sale_amount), 0) AS total_sale,
-    (COALESCE(SUM(i.investment_amount),0) - COALESCE(SUM(i.sale_amount), 0)) AS remaining_sale
-    
+        i.doctor AS doctor,
+        i.town AS town,
+        i.mso AS mso,
+        i.rm AS rm,
+        i.medical_store AS medical_store,
+
+        -- Sum for each month from Investment Entry Items
+        SUM(CASE WHEN i.month = 'January' THEN i.investment_amount ELSE 0 END) AS january_investment,
+        SUM(CASE WHEN i.month = 'February' THEN i.investment_amount ELSE 0 END) AS february_investment,
+        SUM(CASE WHEN i.month = 'March' THEN i.investment_amount ELSE 0 END) AS march_investment,
+        SUM(CASE WHEN i.month = 'April' THEN i.investment_amount ELSE 0 END) AS april_investment,
+        SUM(CASE WHEN i.month = 'May' THEN i.investment_amount ELSE 0 END) AS may_investment,
+        SUM(CASE WHEN i.month = 'June' THEN i.investment_amount ELSE 0 END) AS june_investment,
+        SUM(CASE WHEN i.month = 'July' THEN i.investment_amount ELSE 0 END) AS july_investment,
+        SUM(CASE WHEN i.month = 'August' THEN i.investment_amount ELSE 0 END) AS august_investment,
+        SUM(CASE WHEN i.month = 'September' THEN i.investment_amount ELSE 0 END) AS september_investment,
+        SUM(CASE WHEN i.month = 'October' THEN i.investment_amount ELSE 0 END) AS october_investment,
+        SUM(CASE WHEN i.month = 'November' THEN i.investment_amount ELSE 0 END) AS november_investment,
+        SUM(CASE WHEN i.month = 'December' THEN i.investment_amount ELSE 0 END) AS december_investment,
+
+        COALESCE(SUM(i.investment_amount),0) AS total_investment,
+        COALESCE(SUM(i.sale_target),0) AS total_sale_target,
+
+        -- Sum for each month from Sales Entry Items
+        SUM(CASE WHEN i.month = 'January' THEN i.sale_amount ELSE 0 END) AS january_sales,
+        SUM(CASE WHEN i.month = 'February' THEN i.sale_amount ELSE 0 END) AS february_sales,
+        SUM(CASE WHEN i.month = 'March' THEN i.sale_amount ELSE 0 END) AS march_sales,
+        SUM(CASE WHEN i.month = 'April' THEN i.sale_amount ELSE 0 END) AS april_sales,
+        SUM(CASE WHEN i.month = 'May' THEN i.sale_amount ELSE 0 END) AS may_sales,
+        SUM(CASE WHEN i.month = 'June' THEN i.sale_amount ELSE 0 END) AS june_sales,
+        SUM(CASE WHEN i.month = 'July' THEN i.sale_amount ELSE 0 END) AS july_sales,
+        SUM(CASE WHEN i.month = 'August' THEN i.sale_amount ELSE 0 END) AS august_sales,
+        SUM(CASE WHEN i.month = 'September' THEN i.sale_amount ELSE 0 END) AS september_sales,
+        SUM(CASE WHEN i.month = 'October' THEN i.sale_amount ELSE 0 END) AS october_sales,
+        SUM(CASE WHEN i.month = 'November' THEN i.sale_amount ELSE 0 END) AS november_sales,
+        SUM(CASE WHEN i.month = 'December' THEN i.sale_amount ELSE 0 END) AS december_sales,
+
+        COALESCE(SUM(i.sale_amount), 0) AS total_sale,
+
+        (COALESCE(SUM(i.investment_amount),0) - COALESCE(SUM(i.sale_amount), 0)) AS remaining_sale,
+        "" AS remaining_amount
+
     FROM
         `tabInvestment Entry Items` i
+    WHERE
+        {where_clause}
     GROUP BY
-        i.doctor, i.town, i.mso, i.rm, i.medical_store;
+        i.doctor, 
+        i.town, 
+        i.mso, 
+        i.rm, 
+        i.medical_store;
     """
-    query_result = frappe.db.sql(query, as_dict=True)
-    data.extend(query_result)
-    return data
+
+    query_result = frappe.db.sql(query, parameters, as_dict=True)
+
+    return query_result
